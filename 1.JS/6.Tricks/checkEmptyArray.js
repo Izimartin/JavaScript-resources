e4cb1abf777b150966c082d0d99ddac3ac3bd3e4
Brealy Padron Rodriguez <nigarumovum@Brealys-MacBook-Air.local>
2022-06-23 16:02:30 -0600

    addings

---
+++ 1.JS/6.Tricks/checkEmptyArray.js
@@ -0,0 +1,8 @@
+//If need to find out if it is an array
+
+myArray = "Brealy"
+
+console.log(myArray && myArray.length ? true : false);
+console.log(myArray.length ? true : false);
+
+console.log(Array.isArray(myArray));
